564fde9ac6a295794e40bd5f6071b779006e1271
martholamew <mspringett824@gmail.com>
2024-11-17 19:27:14 -0500

    add authorizations and security filter. this is not ready for production

--- src/main/java/fellowshipofthemovieclub/fellowship/config/SecurityConfig.java
+++ src/main/java/fellowshipofthemovieclub/fellowship/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:7001");
+        configuration.addAllowedOrigin("https://fellowshipofthemovieclub.com/");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
